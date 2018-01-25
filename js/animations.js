28c4101ffcbb83a03bc8964e89eb3ed9b310b252
Natalie Conde Mejía <natalie.conde@moresoft.com>
2018-01-25 12:46:18 -0600

    Fondo, logo, etc

--- js/animations.js
+++ js/animations.js
@@ -1,12 +1,4 @@
- 
 
-function resetNav(){
-	$('#s').removeClass('here');
-	$('#a').removeClass('here');
-	$('#f').removeClass('here');
-	$('#p').removeClass('here');
-	$('#c').removeClass('here');
-}
 
 function htmlbodyHeightUpdate(){
 	var height3 = $( window ).height()
@@ -86,52 +78,4 @@ $(document).ready(function (){
 	    $(window).resize(checkWidth);
 
       //------------------------------------------------//
-});
-
-$(window).scroll(function (event) {
-    var scroll = $(window).scrollTop();
-
-    var Top_INICIO = $('#inicio').position().top;
-    var Top_SOMOS = $('#Somos').position().top;
-
-    var Top_ADN = ($('#ADN').position().top) - 20;
-    var Top_FRAC = ($('#FracIMG').position().top)- 20;
-    var Top_PORQ = ($('#PorQ').position().top)- 20;
-    var Top_ECOT = ($('#EcoT').position().top)- 20;
-    var Top_PTSV = ($('#PtsV').position().top)- 20;
-
-    var Bot_PTSV = $('#PtsV').position().top + $('#PtsV').height();
-    var Top_CONT = $('#Cont').position().top;
-
-
-    if (scroll <= Top_SOMOS) {
-    	resetNav();
-    	$('#s').addClass('here');
-    }
-    else if (scroll <= Top_ADN) {
-    	// console.log('SOMOS');
-    	resetNav();
-    	$('#s').addClass('here');
-    }
-    else if (scroll <= Top_FRAC) {
-    	// console.log('ADN');
-    	resetNav();
-    	$('#a').addClass('here');
-    }
-    else if (scroll <= Top_PORQ) {
-    	// console.log('FRACCIONAMIENTOS');
-    	resetNav();
-    	$('#f').addClass('here');
-    }
-    else if (scroll <= Top_ECOT) {
-    	// console.log('POR QUE');
-    	resetNav();
-    	$('#p').addClass('here');
-    }
-    else if (scroll <= Top_PTSV) {
-    	// console.log('CONT');
-    	resetNav();
-    	$('#c').addClass('here');
-    }
-
 });
