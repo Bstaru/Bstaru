89cf2ea3e4d466722fbfbb6d96c5228f3f206905
Natalie Conde Mejía <natalie.conde@moresoft.com>
2018-02-16 11:28:56 -0600

    Cambios sliders

--- js/scroll.js
+++ js/scroll.js
@@ -22,52 +22,52 @@ $(document).ready(function (){
 
 });
 
-$(window).scroll(function (event) {
-    var scroll = $(window).scrollTop();
+// $(window).scroll(function (event) {
+//     var scroll = $(window).scrollTop();
 
-    var Top_INICIO = $('#inicio').position().top;
-    var Top_SOMOS = $('#Somos').position().top;
+//     var Top_INICIO = $('#inicio').position().top;
+//     var Top_SOMOS = $('#Somos').position().top;
 
-    var Top_ADN = ($('#ADN').position().top) - 20;
-    var Top_FRAC = ($('#FracIMG').position().top)- 20;
-    var Top_PORQ = ($('#PorQ').position().top)- 20;
-    var Top_ECOT = ($('#EcoT').position().top)- 20;
-    var Top_PTSV = ($('#PtsV').position().top)- 20;
+//     var Top_ADN = ($('#ADN').position().top) - 20;
+//     var Top_FRAC = ($('#FracIMG').position().top)- 20;
+//     var Top_PORQ = ($('#PorQ').position().top)- 20;
+//     var Top_ECOT = ($('#EcoT').position().top)- 20;
+//     var Top_PTSV = ($('#PtsV').position().top)- 20;
 
-    var Bot_PTSV = $('#PtsV').position().top + $('#PtsV').height();
-    var Top_CONT = $('#Cont').position().top;
+//     var Bot_PTSV = $('#PtsV').position().top + $('#PtsV').height();
+//     var Top_CONT = $('#Cont').position().top;
 
 
-    if (scroll <= Top_SOMOS) {
-        resetNav();
-        $('#s').addClass('here');
-    }
-    else if (scroll <= Top_ADN) {
-        // console.log('SOMOS');
-        resetNav();
-        $('#s').addClass('here');
-    }
-    else if (scroll <= Top_FRAC) {
-        // console.log('ADN');
-        resetNav();
-        $('#a').addClass('here');
-    }
-    else if (scroll <= Top_PORQ) {
-        // console.log('FRACCIONAMIENTOS');
-        resetNav();
-        $('#f').addClass('here');
-    }
-    else if (scroll <= Top_ECOT) {
-        // console.log('POR QUE');
-        resetNav();
-        $('#p').addClass('here');
-    }
-    else if (scroll <= Top_PTSV) {
-        // console.log('CONT');
-        resetNav();
-        $('#c').addClass('here');
-    }
+//     if (scroll <= Top_SOMOS) {
+//         resetNav();
+//         $('#s').addClass('here');
+//     }
+//     else if (scroll <= Top_ADN) {
+//         // console.log('SOMOS');
+//         resetNav();
+//         $('#s').addClass('here');
+//     }
+//     else if (scroll <= Top_FRAC) {
+//         // console.log('ADN');
+//         resetNav();
+//         $('#a').addClass('here');
+//     }
+//     else if (scroll <= Top_PORQ) {
+//         // console.log('FRACCIONAMIENTOS');
+//         resetNav();
+//         $('#f').addClass('here');
+//     }
+//     else if (scroll <= Top_ECOT) {
+//         // console.log('POR QUE');
+//         resetNav();
+//         $('#p').addClass('here');
+//     }
+//     else if (scroll <= Top_PTSV) {
+//         // console.log('CONT');
+//         resetNav();
+//         $('#c').addClass('here');
+//     }
 
-});
+// });
 
 
